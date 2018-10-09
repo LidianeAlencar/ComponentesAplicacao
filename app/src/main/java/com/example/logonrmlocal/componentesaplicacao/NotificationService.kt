1cb4f485773e8d96f9212eeced98b4466eec1cdb
Logon Aluno <logonrmlocal@fiap.com.br>
2018-10-09 19:53:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/componentesaplicacao/NotificationService.kt
@@ -0,0 +1,11 @@
+package com.example.logonrmlocal.componentesaplicacao
+
+import android.app.IntentService
+import android.content.Intent
+
+class NotificationService : IntentService(""){
+    override fun onHandleIntent(intent: Intent?) {
+        
+
+    }
+}
